eb448c01be59caeccb260f448792dd24e472ac65
mauri0098 <maurirodriguez0098@gmail.com>
2023-06-03 04:05:27 -0300

    26/05/2023

--- SP1Rodriguez/fmrPrincipal.Designer.cs
+++ SP1Rodriguez/fmrPrincipal.Designer.cs
@@ -60,7 +60,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::SP1Rodriguez.Properties.Resources._4GSUPRZXXREO7A3YKFIT3W2RMM;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 98);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 90);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(383, 158);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
